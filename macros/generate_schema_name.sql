de2c7ed06d6cc39a9f4f3494d01f277f7053795d
pawelofficial <bogus2137@icloud.com>
2026-06-24 16:26:05 +0000

    make dbt work

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,23 +1,7 @@
-{% macro generate_schema_name(custom_schema_name, node) %}
-
-    {% set default_schema = target.schema %}
-
-    {# seeds go in a global `raw` schema #}
-    {% if node.resource_type == 'seed' %}
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- if custom_schema_name is none -%}
+        {{ target.schema }}
+    {%- else -%}
         {{ custom_schema_name | trim }}
-
-    {# non-specified schemas go to the default target schema #}
-    {% elif custom_schema_name is none %}
-        {{ default_schema }}
-
-
-    {# specified custom schema names go to the schema name prepended with the the default schema name in prod (as this is an example project we want the schemas clearly labeled) #}
-    {% elif target.name == 'prod' %}
-        {{ default_schema }}_{{ custom_schema_name | trim }}
-
-    {# specified custom schemas go to the default target schema for non-prod targets #}
-    {% else %}
-        {{ default_schema }}
-    {% endif %}
-
-{% endmacro %}
+    {%- endif -%}
+{%- endmacro %}
